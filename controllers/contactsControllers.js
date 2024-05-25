66d8aad8511b1fb8159f73f03d641d332b438fc7
VadymVelenets <128249544+VadymVelenets@users.noreply.github.com>
2024-05-25 23:44:02 +0300

    33

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,60 +2,63 @@ import HttpError from "../helpers/HttpError.js";
 import { addContact, getContactById, listContacts, removeContact, updContact } from "../services/contactsServices.js";
 
 export const getAllContacts = async (req, res, next) => {
-    try {
-        const getContacts = await listContacts();
-        res.send(getContacts);
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const getContacts = await listContacts();
+    res.send(getContacts);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getOneContact = async (req, res, next) => {
-    try {
-        const getContact = await getContactById(req.params.id);
-        if (!getContact) {
-            next (HttpError(404));
-        }
-        res.status(200).send(getContact)
-    } catch (error) {
-        next (error)
+  try {
+    const getContact = await getContactById(req.params.id);
+    if (!getContact) {
+      next(HttpError(404));
     }
+    res.status(200).send(getContact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteContact = async (req, res, next) => {
-    try {
-        const delContact = await removeContact(req.params.id);
-        if (!delContact) {
-            next (HttpError(404));
-        }
-        res.status(200).send(delContact)
-    } catch (error) {
-        next (error)
+  try {
+    const delContact = await removeContact(req.params.id);
+    if (!delContact) {
+      next(HttpError(404));
     }
+    res.status(200).send(delContact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const createContact = async (req, res, next) => {
-    try {
-        const { name, email, phone } = req.body;
-        const newContact = await addContact(name, email, phone);
-        if (!newContact) {
-            next (HttpError(404));
-        };
-        res.status(201).send(newContact)
-    } catch (error) {
-        next (error)
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await addContact(name, email, phone);
+    if (!newContact) {
+      next(HttpError(404));
     }
+    res.status(201).send(newContact);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateContact = async (req, res, next) => {
-    try {
-        const { name, email, phone } = req.body;
-        const updatedContact = await updContact(req.params.id, name, email, phone);
-        if (!updatedContact) {
-            next (HttpError(404));
-        };
-        res.status(200).send(updatedContact)
-    } catch (error) {
-        next (error)
+  try {
+    const { id } = req.params;
+    if (!Object.keys(req.body).length) {
+      return next(HttpError(400, "Body must have at least one field"));
     }
+    const updatedContact = await updContact(id, req.body);
+    if (!updatedContact) {
+      next(HttpError(404));
+    }
+    res.status(200).send(updatedContact);
+  } catch (error) {
+    next(error);
+  }
 };
